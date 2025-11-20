5c2bf93791ed6e88a6ab9ed2b678a0c8d5bd1675
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-11-20 18:07:29 +0530

    anothertask

---
+++ Typescript/condition_loop/breakUse.js
@@ -0,0 +1,6 @@
+for (var i = 1; i <= 25; i++) {
+    if (i === 14) {
+        break;
+    }
+    console.log(i);
+}
---
+++ Typescript/condition_loop/breakUse.ts
@@ -0,0 +1,6 @@
+for (let i = 1; i <= 25; i++) {
+  if (i === 14) {
+    break;
+  }
+  console.log(i);
+}
