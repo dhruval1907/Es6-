5e5c563b772acd7d84b18ee0a75667fcc821ea9e
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-18 22:37:34 +0530

    getbacktowork

--- Weather-api/script.js
+++ Weather-api/script.js
@@ -1,13 +1,14 @@
 let apikey = `1cc953bb42da005a51a8ee271cc092f6`
-let apiurl = `https://api.openweathermap.org/data/2.5/weather?units=metric%q=banglore`
+let apiurl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore
+`
 
 
 async function checkweather() {
     const response = await fetch(apiurl + `&appid=${apikey}`)
 
     var data = await response.json();
- 
-console.log(data);
+
+    console.log(data);
 
 }
 
