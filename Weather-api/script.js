20e7c203af054dc873d9853c62db277995d937b4
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-18 23:02:37 +0530

    weatherapp

--- Weather-api/script.js
+++ Weather-api/script.js
@@ -1,10 +1,13 @@
+// Weather API
 let apikey = `1cc953bb42da005a51a8ee271cc092f6`
-let apiurl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=
 
+// Weather URl
+let apiurl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=
 `
+// accessing the element
 let search = document.querySelector(".search input")
 let btn = document.querySelector("#btn")
-
+// fetching the data
 async function checkweather(city) {
     const response = await fetch(apiurl +city +  `&appid=${apikey}`)
 
