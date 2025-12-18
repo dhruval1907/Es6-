b47fadc559cbc2b6160c2eb94ad393b51aaed4bb
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-18 22:55:04 +0530

    getbacktowork

--- Weather-api/script.js
+++ Weather-api/script.js
@@ -1,16 +1,20 @@
 let apikey = `1cc953bb42da005a51a8ee271cc092f6`
-let apiurl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore
+let apiurl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=
+
 `
-async function checkweather() {
-    const response = await fetch(apiurl + `&appid=${apikey}`)
+let search = document.querySelector(".search")
+let btn = document.querySelector("#btn")
+
+async function checkweather(city) {
+    const response = await fetch(apiurl +city +  `&appid=${apikey}`)
 
     var data = await response.json();
 
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "% ";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
 checkweather()
