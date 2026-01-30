ae80e24c6484e926ccd833c3a1dcae65638daa4c
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-30 23:20:56 +0530

    creating tasks

---
+++ react/task/src/App.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react'
+import Card from './components/Card'
+
+const App = () => {
+
+  const [task, settask] = useState([])
+  const [description, setDescription] = useState([])
+  const [alldata, setalldata] = useState([])
+
+  function formHandler(e) {
+    e.preventDefault()
+    const allData = {
+      task,
+      description
+    }
+    setalldata((prev) => {
+      return [...prev, allData]
+    })
+    console.log(alldata);
+    settask("")
+    setDescription("")
+  }
+
+  useEffect(() => {
+    const savedData = localStorage.getItem("tasks")
+
+    if (savedData) {
+      setalldata(JSON.parse(savedData))
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(alldata))
+  }, [alldata])
+
+
+  return (
+    <div className='h-screen w-full ' style={{ background: "linear-gradient( to top,white,lightblue,crimson)" }}>
+      <div className='main w-full h-screen flex justify-around' style={{ padding: "2rem" }}>
+        <div className='left w-[40%] h-full bg-zinc-800 rounded' style={{ padding: "1.5rem" }}>
+          <h1 className='font-semibold text-white'>Task Manager</h1>
+          <br />
+          <form
+            onSubmit={(e) => {
+              formHandler(e)
+            }}
+            className='flex flex-col items-start'>
+            <div className='flex items-center gap-6'>
+              <label htmlFor="" className='text-red-100 font-bold'>Add a task  :          </label>
+              <input
+                onChange={(e) => {
+                  settask(e.target.value)
+                }}
+                type="text" className='border-2 border-red-200 rounded text-white font-semibold w-45' style={{ padding: "5px" }} />
+            </div>
+            <br /> <br />
+            <div className='flex items-start gap-5'>
+              <label htmlFor="" className='text-red-100 font-bold'>  Description     :          </label>
+              <textarea
+                onChange={(e) => {
+                  setDescription(e.target.value)
+                }}
+                name="" id="" className='border-2 w-43  border-red-200 rounded text-white font-semibold h-20'></textarea>
+            </div>
+            <br /> <br />
+            <div className='flex items-center w-full justify-center'>
+              <button className='active:scale-95   bg-zinc-400 rounded font-bold ' style={{ padding: "0.7rem 2.5rem" }}>Submit</button>
+            </div>
+          </form>
+        </div>
+        <div className='left w-[40%] h-full bg-zinc-400 rounded overflow-y-auto flex flex-wrap justify-evenly sh' style={{ paddingTop: "20px" }}>
+          <div className='overflow-y-auto w-full flex justify-evenly gap-2 flex-wrap shrink-0'>
+            {alldata.map((value,indx) => {
+              return <Card key={indx} data={alldata} />
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ react/task/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = (props) => {
+    
+    console.log(props)
+    return (
+    <div className='max-h-30 w-40 bg-blue-400/70 rounded wrap-break-word text-zinc-900/70 font-bold whitespace-wrap' style={{padding:"0.5rem"}}>
+        <h1>{props}</h1>
+        <p className='' style={{marginTop:"10px"}}>{props.description}</p>
+    </div>
+  )
+}
+
+export default Card
