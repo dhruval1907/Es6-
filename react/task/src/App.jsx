d4efd512e36541550c596dea04b631167e78041e
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-30 23:27:26 +0530

    useeffect

--- react/task/src/App.jsx
+++ react/task/src/App.jsx
@@ -70,8 +70,8 @@ const App = () => {
         </div>
         <div className='left w-[40%] h-full bg-zinc-400 rounded overflow-y-auto flex flex-wrap justify-evenly sh' style={{ paddingTop: "20px" }}>
           <div className='overflow-y-auto w-full flex justify-evenly gap-2 flex-wrap shrink-0'>
-            {alldata.map((value,indx) => {
-              return <Card key={indx} data={alldata} />
+            {alldata.map((item) => {
+              return <Card key={item.id} data={item} />
             })}
           </div>
         </div>
--- react/task/src/components/Card.jsx
+++ react/task/src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Card = (props) => {
+const Card = ({data}) => {
     
     console.log(props)
     return (
     <div className='max-h-30 w-40 bg-blue-400/70 rounded wrap-break-word text-zinc-900/70 font-bold whitespace-wrap' style={{padding:"0.5rem"}}>
-        <h1>{props}</h1>
+        <h1>{data.task}</h1>
         <p className='' style={{marginTop:"10px"}}>{props.description}</p>
     </div>
   )
